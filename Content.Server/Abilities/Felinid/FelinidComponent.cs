713d9cb363d818f1c64ad1a35363862d38c72412
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-06-13 10:11:50 -0400

    Ported felinids from Nyanotrasen

---
+++ Content.Server/Abilities/Felinid/FelinidComponent.cs
@@ -0,0 +1,23 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;
+using Content.Shared.Actions;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Abilities.Felinid;
+
+[RegisterComponent]
+public sealed partial class FelinidComponent : Component
+{
+    /// <summary>
+    /// The hairball prototype to use.
+    /// </summary>
+    [DataField("hairballPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string HairballPrototype = "Hairball";
+
+    [DataField("hairballAction")]
+    public EntityUid? HairballAction = null;
+
+    public EntityUid? EatActionTarget = null;
+
+    public EntityUid? EatAction = null;
+}
